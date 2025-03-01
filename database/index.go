051d4f998e187a4422b68d3986b3e6226a23bb93
water2027 <136900643+water2027@users.noreply.github.com>
2025-03-01 18:29:39 +0800

    env

--- database/index.go
+++ database/index.go
@@ -3,13 +3,17 @@ package database
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println(err)
-		return
+	env := os.Getenv("GO_ENV");
+	if env != "docker" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
 	initMysqlDB()
 	initRedisClient()
